6eaccf9c707e49a56ffe0e12853181c364c6b7ce
aliva <ali.vakilzade@gmail.com>
2011-08-28 20:05:35 +0430

    can authorize identi.ca

--- microblogger.py
+++ microblogger.py
@@ -22,12 +22,15 @@ class MicrobloggerConfigurable(GObject.Object, PeasGtk.Configurable):
     __gtype_name__ = 'MicrobloggerConfigurable'
 
     def do_create_configure_widget(self):
+        self.request = Requests()
         ui_file = rb.find_plugin_file(self, "microblogger-prefs.ui")
         self.builder = Gtk.Builder()
         self.builder.add_from_file(ui_file)
 
         self.builder.get_object('add_account').connect('clicked', self.on_add_account_clicked)
         self.builder.get_object('del_account').connect('clicked', self.on_del_account_clicked)
+
+        self.builder.get_object('authorize').connect('clicked', self.on_authorize_clicked)
 
         self.builder.get_object('cancel').connect('clicked', self.on_cancel_clicked)
 
@@ -43,3 +46,19 @@ class MicrobloggerConfigurable(GObject.Object, PeasGtk.Configurable):
 
     def on_cancel_clicked(self, button):
         self.builder.get_object('general').set_current_page(0)
+
+    def on_authorize_clicked(self, button):
+        self.request.authorize(self.builder.get_object('note')) 
+
+class Requests:
+    def __init__(self):
+        pass
+
+    def authorize(self, note):
+        note.set_text('start')
+        import oauth2 as o
+        key, sec = ('79c5e601c6d3154d4a19014ffb51e6f9', 'c82bbbd875eebc6efd807910b873ea09')
+        con = o.Consumer(key, sec)
+        cli = o.Client(con)
+        resp, cont = cli.request('https://identi.ca/api/oauth/request_token?oauth_callback=oob')
+        note.set_text(str(resp.status) +'\n'+cont)
